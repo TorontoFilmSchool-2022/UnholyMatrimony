f0fd66054913d7e3ddbb87e9a0c6ed6fcd36f33d
Brett Penney <103518828+BPenney98@users.noreply.github.com>
2022-10-21 00:55:35 -0500

    Navmesh Baked
    Created a enemy walker place holder and baked a navmesh.

---
+++ Assets/Scripts/Walker.cs
@@ -0,0 +1,66 @@
+// <header>
+// Description:  Walker behavior script.
+// Created By  : Brett Penney   (Brett.Penney@hotmail.com)
+// Created On  : 2022/10/21
+// Modified By : 
+// Modified On : 
+// Modification Note:
+// Other Notes:
+// </header>
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Walker : MonoBehaviour
+{
+
+	#region Static and Private variables/Fields/Properties
+
+
+	#endregion
+
+
+
+	#region Public/Inspector variables
+
+
+	#endregion
+
+
+
+	#region Private Methods - Unity methods
+
+	void Awake()
+	{
+
+	}
+
+	// Start is called before the first frame update
+	void Start()
+	{
+
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+
+	}
+
+	#endregion
+
+
+
+	#region Private Methods
+
+	#endregion
+
+
+
+	#region Public methods
+
+	#endregion
+
+}
